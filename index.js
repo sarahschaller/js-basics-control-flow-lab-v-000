5dabdfc3819039d2e5dde4d278c26b0fc40a9da2
Sarah Schaller <sarahcschaller@gmail.com>
2018-05-28 22:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!';
   } else if (2000 < feet && feet < 2500) {
-    return 'I will glady take your thirty bucks.';
+    return 'I will gladly take your thirty bucks.';
   } else {
     return 'No can do.';
   }
